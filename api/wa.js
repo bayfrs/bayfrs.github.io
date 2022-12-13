8394e7b6ddfee16bd0a884d06d5b182e105a6148
bayfrs <113220899+bayfrs@users.noreply.github.com>
2022-12-13 00:33:12 -0800

    Update wa.js

--- api/wa.js
+++ api/wa.js
@@ -1,5 +1,5 @@
 
 		var timer = setTimeout(function() {
-            window.location='https://api.whatsapp.com/send?phone=6283823131870&amp;text=Halo%20Admin,%20Saya%20Mau%20Pesan'
+            window.location='https://web.whatsapp.com/send/?phone=6283823131870&amp%3Btext=Halo+Admin%2C+Saya+Mau+Pesan'
         }, 4000);
 	
