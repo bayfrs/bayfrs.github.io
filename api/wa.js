13422c03f25cd5c28e3cd26d2d2c473c0cd2dc47
bayfrs <bayfrs@gmail.com>
2026-02-01 15:10:45 +0700

    bayfrs

---
+++ api/wa.js
@@ -0,0 +1,5 @@
+
+		var timer = setTimeout(function() {
+            window.location='https://web.whatsapp.com/send/?phone=6283823131870&amp%3Btext=Halo+Admin%2C+Saya+Mau+Pesan'
+        }, 4000);
+	
